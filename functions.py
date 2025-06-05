5a4a189e2e7260d1fc80f5e2dec6c8d6a5dd562e
juliusmayr <mj4127@mci4me.at>
2025-06-05 12:41:23 +0200

    Added,plotly,numpy,pandas; created diagramm(Verbrachte Zeit in Power-Stufe)

---
+++ functions.py
@@ -0,0 +1,22 @@
+#%%
+import pandas as pd
+import numpy as np
+import plotly.express as px
+# %%
+#Tabelle erstellen
+df = pd.read_csv('data/activity.csv')
+df_select = df[['PowerOriginal','Duration', 'HeartRate' ]]
+print(df_select)
+# %%
+#Sortieren der Tabelle nach Power und Zeit
+power_duration = df_select.groupby('PowerOriginal')['Duration'].sum().reset_index()
+print(power_duration)
+
+# %%
+#Diagramm erstellen
+fig = px.bar(power_duration, x='Duration', y='PowerOriginal',
+             labels={'PowerOriginal': 'Power [W]', 'Duration': 'Zeit [s]'},
+             title='Verbrachte Zeit pro Power-Stufe')
+fig.show()
+    
+# %%
