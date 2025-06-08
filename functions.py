c484279e665c0413a91dcde29606899d09bfb45e
MaierElias <me7770@mci4me.at>
2025-06-08 18:48:40 -0400

    modified code to calculate the right durations.

--- functions.py
+++ functions.py
@@ -7,16 +7,35 @@ import plotly.express as px
 df = pd.read_csv('data/activity.csv')
 df_select = df[['PowerOriginal','Duration', 'HeartRate' ]]
 print(df_select)
-# %%
-#Sortieren der Tabelle nach Power und Zeit
-power_duration = df_select.groupby('PowerOriginal')['Duration'].sum().reset_index()
-print(power_duration)
 
 # %%
-#Diagramm erstellen
-fig = px.scatter(power_duration, x='Duration', y='PowerOriginal',
-             labels={'PowerOriginal': 'Power [W]', 'Duration': 'Zeit [s]'},
-             title='Verbrachte Zeit pro Power-Stufe')
+# Hier wird der DataFrame gefiltert um die Leistungskurve zu erstellen
+max_duration = df_select["Duration"].sum()  # z.B. bis 1 Stunde
+step = 1  # Schrittweite in Sekunden
+
+durations = []
+max_powers = []
+
+for window in range(1, max_duration + 1, step):
+    rolling = df_select['PowerOriginal'].rolling(window, min_periods=window).mean()
+    max_power = rolling.max()
+    if not np.isnan(max_power):
+        durations.append(window)
+        max_powers.append(max_power)
+
+# %%
+import plotly.graph_objects as go
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=np.array(durations) /60, # Umwandlung von Sekunden in Minuten 
+                        y=max_powers,
+                        mode='lines',
+                        name='Power Duration Curve'))
+fig.update_layout(
+    xaxis_title='Dauer [min]',
+    yaxis_title='Leistung [W]',
+    title='Leistungskurve (Power Duration Curve)',
+    xaxis_type='log'
+)
 fig.show()
-    
+
 # %%
