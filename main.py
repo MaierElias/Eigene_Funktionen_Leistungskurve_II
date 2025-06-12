bfb104316cb3858e6c4536069790c2bbd0208537
MaierElias <me7770@mci4me.at>
2025-06-12 12:37:47 -0400

    fixed an error that would cause the script to fail, when the dataframe should not be printed

--- main.py
+++ main.py
@@ -9,17 +9,15 @@ input_data = input("Bitte geben Sie den Pfad zur auszuwertenden Datei ein (CSV o
 print("Eingabe:", input_data)
 
 created_table = create_table(input_data)
+df, fig = create_leistungskurve(created_table)
 
 ask_for_dataframe = input("Möchten Sie die den Dataframe anzeigen? (ja/nein): ").strip().lower()
 if ask_for_dataframe == "ja":
-    df, fig = create_leistungskurve(created_table)
-
     print(df)
 
 ask_for_plot = input("Möchten Sie die Leistungskurve plotten? (ja/nein): ").strip().lower()
 if ask_for_plot == "ja":
     print("Die Leistungskurve wird jetzt erstellt...")
-    create_leistungskurve(created_table)
     fig.show(renderer='browser') # Anzeige im Browser
 else: 
     print("Vielen Dank für die Nutzung des Programms!")
