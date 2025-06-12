55532369eb84758be63ab2665c47997f5cf98aa8
MaierElias <me7770@mci4me.at>
2025-06-12 08:22:05 -0400

    added leistungskurve.png to figures

--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
-#%%
+
 import pandas as pd
 import numpy as np
-import plotly.express as px
-# %%
+import plotly.graph_objects as go
+
 #Tabelle erstellen
 df = pd.read_csv('data/activity.csv')
 df_select = df[['PowerOriginal','Duration', 'HeartRate' ]]
-print(df_select)
+# print(df_select)
 
-# %%
+
 # Hier wird der DataFrame gefiltert um die Leistungskurve zu erstellen
 max_duration = df_select["Duration"].sum()  # z.B. bis 1 Stunde
 step = 1  # Schrittweite in Sekunden
@@ -23,8 +23,7 @@ for window in range(1, max_duration + 1, step):
         durations.append(window)
         max_powers.append(max_power)
 
-# %%
-import plotly.graph_objects as go
+
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=np.array(durations) /60, # Umwandlung von Sekunden in Minuten 
                         y=max_powers,
@@ -40,6 +39,4 @@ fig.update_layout(
         ticktext=['0.02', '0.05', '0.1', '0.2', '0.5', '1', '2', '5', '10', '20']
     )
 )
-fig.show()
-
-# %%
+fig.show(renderer='browser')  # 'png' for static image, 'browser' for interactive plot in browser
